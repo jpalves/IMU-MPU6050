e7cb60e833952eef3aadc28cc50277893cda83ff
João Pedro Alves <jpalves.unico@gmail.com>
2016-08-12 10:32:39 +0100

    Em testes

--- AHRS6/AHRS.h
+++ AHRS6/AHRS.h
@@ -5,20 +5,18 @@ float invSqrt(float x);
 
 class MahonyAHRS{
   		float q0, q1, q2, q3;
-                float qw, qx, qy, qz;
 		float T, twoKp, twoKi;
                 float integralFBx,  integralFBy, integralFBz;
 	public:
     		MahonyAHRS(){
                         integralFBx = 0.0f,  integralFBy = 0.0f, integralFBz = 0.0f;
-                        qw = 0, qx = 0, qy=0, qz = 0;
                 }
                 void mahonyAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az);
 		inline float &TAmostragem(){
 			return T;	
 		}
-		inline float &getQ0(){
-			return q0;		
+		inline float &getQ0(){	
+                        return q0;		
 		}
 		inline float &getQ1(){
 			return q1;		
